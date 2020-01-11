e09669848363f57c7f4a7c2a4c4c79c258772fcd
Christian Nascimento <cnascimentobr@gmail.com>
2020-01-11 04:44:24 -0300

    Add repository

--- src/App.js
+++ src/App.js
@@ -8,6 +8,17 @@ function App() {
     <StyledApp>
       <h2>React Piano App</h2>
       <h1>A Funny piano made with React</h1>
+      <div className="repository">
+        Repository:&nbsp;
+        <a
+          href="https://github.com/cbfn/react-piano"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Show me the code"
+        >
+          https://github.com/cbfn/react-piano
+        </a>
+      </div>
       <PianoBoard />
     </StyledApp>
   );
@@ -17,12 +28,23 @@ const StyledApp = styled.div`
   text-align: center;
   color: #fff;
   h1 {
-    margin-bottom: 50px;
+    margin-bottom: 10px;
     font-weight: 300;
     margin-top: 0;
   }
   h2 {
     margin-bottom: 0px;
+  }
+  .repository {
+    margin-bottom: 40px;
+    a {
+      color: #fff;
+      display: inline-block;
+      text-decoration: none;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
   }
 `;
 
